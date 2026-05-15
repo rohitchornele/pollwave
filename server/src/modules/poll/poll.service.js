1047e47fe39fb26a52ffe1a31e617f97a9f23b5c
Rohit Chornele <rohitchornele@gmail.com>
2026-05-16 00:58:29 +0530

    jwt fixes

--- server/src/modules/poll/poll.service.js
+++ server/src/modules/poll/poll.service.js
@@ -1,49 +1,66 @@
 import Poll from "./poll.model.js";
+import ApiError from "../../common/utils/api-error.js";
 
-export const createPoll = async (payload) => {
-    console.log("poll payload : ", payload)
-    const poll = await Poll.create(payload);
-    console.log("created poll : ", poll)
+export const createPoll = async (
+    payload
+) => {
+
+    const poll =
+        await Poll.create(payload);
+
     return poll;
 };
 
 export const getAllPolls = async () => {
 
     const polls = await Poll.find({
-        status: "active"
-    }).populate("createdBy", "name avatar").sort({ createdAt: -1 });
+        status: "active",
+    })
+        .populate(
+            "createdBy",
+            "name avatar"
+        )
+        .sort({ createdAt: -1 });
 
     return polls;
 };
 
-export const getPollById = async (pollId) => {
+export const getPollById = async (
+    pollId
+) => {
 
-    const poll = await Poll.findById(pollId)
-        .populate("createdBy", "name avatar");
+    const poll =
+        await Poll.findById(pollId)
+            .populate(
+                "createdBy",
+                "name avatar"
+            );
 
     if (!poll) {
-        return {
-            status: 400,
-            message: "Something went wrong"
-        }
+        throw ApiError.notFound(
+            "Poll not found"
+        );
     }
 
     return poll;
 };
 
-export const deletePoll = async (pollId) => {
+export const deletePoll = async (
+    pollId
+) => {
 
-    const poll = await Poll.findById(pollId);
+    const poll =
+        await Poll.findById(pollId);
 
     if (!poll) {
-        // throw new Error("Poll not found");
-        return {
-            status: 400,
-            message: "Something went wrong"
-        }
+        throw ApiError.notFound(
+            "Poll not found"
+        );
     }
 
     poll.status = "closed";
 
     await poll.save();
+
+    return poll;
 };
