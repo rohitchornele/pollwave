115c2abba2a0567dc4cd78dac536687784b9d0e2
Rohit Chornele <rohitchornele@gmail.com>
2026-05-16 01:12:51 +0530

    jwt fixes

--- frontend/src/services/pollService.js
+++ frontend/src/services/pollService.js
@@ -1,44 +1,125 @@
 import { api } from "./api";
 
+const handleError = (error) => {
+
+  console.error(
+    "API ERROR =",
+    error?.response || error
+  );
+
+  throw {
+    status:
+      error?.response?.status || 500,
+
+    message:
+      error?.response?.data?.message ||
+      error.message ||
+      "Something went wrong",
+
+    data:
+      error?.response?.data || null,
+  };
+};
+
 const pollService = {
 
   createPoll: async (pollData) => {
-    console.log("polldata = ", pollData)
-    const response = await api.post("/polls", pollData);
-    return response.data;
+
+    try {
+
+      console.log(
+        "pollData = ",
+        pollData
+      );
+
+      const response =
+        await api.post(
+          "/polls",
+          pollData
+        );
+
+      return response.data;
+
+    } catch (error) {
+
+      handleError(error);
+    }
   },
 
   getAllPolls: async () => {
-    const response = await api.get("/polls");
-    console.log("response = ", response)
-    return response.data.data;
+
+    try {
+
+      const response =
+        await api.get("/polls");
+
+      console.log(
+        "response = ",
+        response
+      );
+
+      return response.data;
+
+    } catch (error) {
+
+      handleError(error);
+    }
   },
 
   getPollById: async (pollId) => {
-    const response = await api.get(`/polls/${pollId}`);
-    return response.data;
+
+    try {
+
+      const response =
+        await api.get(
+          `/polls/${pollId}`
+        );
+
+      return response.data;
+
+    } catch (error) {
+
+      handleError(error);
+    }
   },
 
-  updatePoll: async (pollId, pollData) => {
-    const response = await api.put(`/polls/${pollId}`, pollData);
-    return response.data;
+  updatePoll: async (
+    pollId,
+    pollData
+  ) => {
+
+    try {
+
+      const response =
+        await api.put(
+          `/polls/${pollId}`,
+          pollData
+        );
+
+      return response.data;
+
+    } catch (error) {
+
+      handleError(error);
+    }
   },
 
   deletePoll: async (pollId) => {
-    const response = await api.delete(`/polls/${pollId}`);
-    return response.data;
+
+    try {
+
+      const response =
+        await api.delete(
+          `/polls/${pollId}`
+        );
+
+      return response.data;
+
+    } catch (error) {
+
+      handleError(error);
+    }
   },
-
-//   getTrendingPolls: async () => {
-//     const response = await api.get("/polls/trending");
-//     return response.data;
-//   },
-
-//   searchPolls: async (query) => {
-//     const response = await api.get(`/polls/search?q=${query}`);
-//     return response.data;
-//   },
-
 };
 
 export default pollService;
